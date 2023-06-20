61be4373cc57603a431d6a78e02cbef66f23284a
Glen Sturge <117656215+glen-sturge@users.noreply.github.com>
2023-06-20 09:55:11 -0230

    update logger, month dir, log location

--- logger.js
+++ logger.js
@@ -1,4 +1,4 @@
-const { format, getYear } = require("date-fns");
+const { format, getYear, getMonth } = require("date-fns");
 const { v4: uuid } = require("uuid");
 
 const EventEmitter = require("events");
@@ -20,17 +20,24 @@ class Logger extends EventEmitter {
     const logItem = `${timeStamp}\t${level}\t${event}\t${msg}\t${uuid()}\n`;
     // folder creation
     try {
-      const currentDir = "logs/" + getYear(new Date());
+      const currentYearDir = "logs/" + getYear(new Date());
+      const currentMonthDir = currentYearDir + "/" + getMonth(new Date());
       // if 'logs/' doesn't exist, make it
       if (!fs.existsSync(path.join(__dirname, "logs/")))
-        fsP.mkdir(path.join(__dirname, "logs/"));
+        await fsP.mkdir(path.join(__dirname, "logs/"));
       // if subdir in 'logs/' for the current year doesn't exist, make it.
-      if (!fs.existsSync(path.join(__dirname, currentDir)))
-        fsP.mkdir(path.join(__dirname, currentDir));
+      if (!fs.existsSync(path.join(__dirname, currentYearDir)))
+        await fsP.mkdir(path.join(__dirname, currentYearDir));
+      // if subdir in 'logs/<currentYear>' for the current month doesn't exist, make it.
+      if (!fs.existsSync(path.join(__dirname, currentMonthDir)))
+        await fsP.mkdir(path.join(__dirname, currentMonthDir));
       // formated file name with date.
       const fileName = `${format(new Date(), "yyy-MM-dd")}` + "_events.log";
       // if file doesn't exist, will be created, otherwise logItem will be appended.
-      await fsP.appendFile(path.join(__dirname, "logs", fileName), logItem);
+      await fsP.appendFile(
+        path.join(__dirname, currentMonthDir, fileName),
+        logItem
+      );
     } catch (error) {
       console.log("Problem encountered logging event: " + error);
     }
